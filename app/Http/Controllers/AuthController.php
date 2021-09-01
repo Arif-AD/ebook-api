1fb7f3002e37f1393801d1c56ccd421ae4b9f34e
Arif-AD <arifdestianto937@gmail.com>
2021-09-01 07:03:42 +0700

    migration

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function me()
+    {
+        return 
+        [
+            "NIS" => 3103119028,
+            "Nama" => "Arif Destianton",
+            "Gender" => "Laki-laki",
+            "Phone" => 6281215688821,
+            "Class" => "XII RPL 1"
+        ];
+    }
+}
